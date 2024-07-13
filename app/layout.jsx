ce5763b8d82a3ac5a16da88368c83d11285914f5
Alberto Carrizo <ae_carrizo@hotmail.com>
2024-07-13 19:33:57 -0300

    Cambio de título

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,8 +6,8 @@ const defaultUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` 
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "CMS para Restaurant",
+  description: "CMS para gestión de resto y menú",
 };
 
 export default function RootLayout({ children }) {
