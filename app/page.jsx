6f5059fb35b0281c2dad6a51fd503c4ed8038c72
Alberto Carrizo <ae_carrizo@hotmail.com>
2024-09-05 16:52:01 -0300

    Presentación

--- app/page.jsx
+++ app/page.jsx
@@ -22,26 +22,51 @@ export default async function Index() {
   const isSupabaseConnected = canInitSupabaseClient();
 
   return (
-    <div className="flex-1 w-9/12 flex flex-col gap-10 items-start">
-      <div className="w-full border-t border-zinc-500 p-2 flex justify-center text-center mt-4"></div>
-      <p className="text-2xl">
-        Bienvenido a SuResto:
-      </p>
-      <p className="text-xl">
-        Moderniza tu restaurante o tienda de comidas con un menú dinámico y digital mientras ahorras en impresiones de menús.
-      </p>
-      <p className="text-sm">
-        Optimiza la experiencia de tus clientes con nuestro software,
-        que permite crear un menú accesible desde cualquier dispositivo móvil con lector de códigos QR. Con esta herramienta,
-        podrás personalizar tu menú con el nombre de tu restaurante y organizarlo por categorías como pizzas,
-        sándwiches, entre otros. Dentro de cada categoría, podrás añadir los platos correspondientes con su descripción, precio, foto y alergenos.
+    <div className="w-full flex flex-col gap-10 items-start">
+      <div className="w-full flex flex-col md:flex-row bg-zinc-700">
+        <div className=" text-center my-auto mx-12 p-8">
+          <b className="md:text-6xl text-4xl">
+            Bienvenidos a:
+          </b>
+          <br />
+          <b className="md:text-8xl text-6xl text-center my-auto">
+            SuResto:
+          </b>
+        </div>
+        <div className="flex justify-center w-full">
+          <img src="/FondoIntro.png" className="md:w-3/4 w-72 p-4" alt="imgenPresentacion" />
+        </div>
+      </div>
+      <div className="mx-4">
+        
+        <p className="text-4xl">
+          Creá tu carta QR
+        </p>
+        <br />
+        <b className="md:text-xl text-md">
+          Moderniza tu restaurante o tienda de comidas con un menú dinámico y digital mientras ahorras en impresiones de menús.
+        </b>
+        <ul role="list" class="marker:text-sky-400 list-disc pl-5 space-y md:text-xl text-md">
+        <li>Optimiza la experiencia de tus clientes.</li>
+          <li>Con SuResto podras crear un menú accesible desde cualquier dispositivo móvil con lector de códigos QR.</li> 
+          <li>Con esta herramienta,
+          podrás personalizar tu menú con el nombre de tu restaurante y organizarlo por categorías como pizzas,
+          sándwiches, entre otros. Dentro de cada categoría, podrás añadir los platos correspondientes con su descripción, precio, foto y alergenos.
+          </li>
 
-        Una vez configurado, el sistema generará un código QR único que podrás descargar
-        e imprimir en el soporte de tu elección para ponerlo a disposición de tus clientes.
-        Al escanearlo, tus clientes accederán al menú completo y actualizado en su dispositivo móvil.
-        Además, si alguno de tus menús no está disponible temporalmente, podrás ocultarlo fácilmente desde la aplicación.
-
-      </p>
+          <li>Una vez configurado, el sistema generará un código QR único que podrás descargar
+          e imprimir en el soporte de tu elección para ponerlo a disposición de tus clientes.
+          </li>
+          <li>Al escanearlo, tus clientes accederán al menú completo y actualizado en su dispositivo móvil.
+          Además, si alguno de tus menús no está disponible temporalmente, podrás ocultarlo fácilmente desde la aplicación.
+        </li>
+        </ul>
+        <br />
+        <p className="md:text-xl text-md text-center">
+          Puedes probarlo usando un usuario y contraseñas de muestra:<br />
+          Usuario: wottan@live.com.ar<br />Contraseña: Vercel2024
+        </p>
+      </div>
       <div className="w-full flex justify-center items-center p-3 text-md">
         {isSupabaseConnected &&
           <>
@@ -53,20 +78,20 @@ export default async function Index() {
         <div>
           <div className="flex flex-row text-center">
             <div className="basis-1/3">
-            <b>Desarrollador:</b>
-            <p>Alberto Carrizo</p>
+              <b>Desarrollador:</b>
+              <p>Alberto Carrizo</p>
             </div>
             <div className="basis-1/3">
-            <b>React Framework:</b>
-            <p>Hecho con NEXT.js</p>
+              <b>React Framework:</b>
+              <p>Hecho con NEXT.js</p>
             </div>
             <div className="basis-1/3 flex flex-row relative justify-center">
-              <img src="/linkedin.png" alt="logo" width={24} height={24} className="absolute mr-20 object-scale-down"/>
+              <img src="/linkedin.png" alt="logo" width={24} height={24} className="absolute mr-20 object-scale-down" />
               <a
-              href="https://www.linkedin.com/in/alberto-edelmiro-carrizo-7639a186/"
-              target="_blank"
-              className=" ml-8 font-bold hover:underline"
-              rel="noreferrer"
+                href="https://www.linkedin.com/in/alberto-edelmiro-carrizo-7639a186/"
+                target="_blank"
+                className=" ml-8 font-bold hover:underline"
+                rel="noreferrer"
               >Mi Linkedin</a>
             </div>
 
