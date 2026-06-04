af3f6d11869808bd4ce58d13944708cc156980f1
Alberto Carrizo <ae_carrizo@hotmail.com>
2026-06-04 20:08:51 -0300

    Analytics and images

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,52 @@
-//import { GeistSans } from "geist/font/sans";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
-// Metadatos para SEO de la pagina ya deployada
-const defaultUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000";
+const defaultUrl = process.env.NEXT_PUBLIC_SITE_URL
+  ? process.env.NEXT_PUBLIC_SITE_URL
+  : "http://localhost:3000";
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
   title: "CMS para Restaurant",
   description: "CMS para gestión de resto y menú",
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  alternates: {
+    canonical: defaultUrl,
+    languages: {
+      "es-AR": `${defaultUrl}`,
+    },
+  },
+  openGraph: {
+    title: "Genere su propia cartilla o menú digital",
+    description:
+      "Arme su propia carta de menú digital y comparta el código QR para el cliente",
+    siteName: defaultUrl,
+    images: [
+      {
+        url: "https://turesto.edelbyte.com.ar/turesto.webp",
+        width: 1200,
+        height: 630,
+        alt: "Imagen de la app web turestó",
+      },
+    ],
+    locale: "es-AR",
+    type: "website",
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -20,6 +59,7 @@ export default function RootLayout({ children }) {
         <main className="min-h-screen flex flex-col items-center">
           {children}
         </main>
+        <Analytics />
       </body>
     </html>
   );
